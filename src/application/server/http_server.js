7fd410dafb11e639603a4acdc43c85930b66a020
chooie <charlie.hebert92@gmail.com>
2018-06-25 11:09:47 -0600

    Initial commit

---
+++ src/application/server/http_server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const util = require("util");
+
+exports.make = function make(portNumber, contentDir, notFoundPageToServe) {
+  const httpServer = express();
+
+  httpServer.set("views", "src/application/client/content/template_views");
+  httpServer.set("view engine", "pug");
+
+  httpServer.get("/", function(req, res) {
+    res.render("index", { title: "Home - Automatopia NodeJS" });
+  });
+
+  httpServer.use(express.static(contentDir));
+  httpServer.use(function(req, res, next) {
+    res
+      .status(404)
+      .render("404", { title: "Page Not Found - Automatopia NodeJS" });
+  });
+
+  return {
+    start() {
+      const listenFn = httpServer.listen.bind(httpServer);
+      const listenPromise = util.promisify(listenFn);
+      return listenPromise(portNumber);
+    },
+
+    stop() {
+      const closeFn = httpServer.close.bind(httpServer);
+      const closePromise = util.promisify(closeFn);
+      return closePromise();
+    },
+
+    getHttpServer() {
+      return httpServer;
+    }
+  };
+};
