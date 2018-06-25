c704efb2b06f415ed311e6d4d398017f70a5f59d
chooie <charlie.hebert92@gmail.com>
2018-06-25 12:01:05 -0600

    Can handle an empty head and body

--- src/application/shared/_main_test.js
+++ src/application/shared/_main_test.js
@@ -3,9 +3,14 @@ const main = require("./main.js");
 
 describe("SHARED: Main", function() {
   it("gets valid html", function() {
-    const structureToConvert = [];
+    const structureToConvert = [
+      ["head"],
+      ["body"],
+    ];
     const expectedHtml =
           "<html>" + "\n" +
+          "  <head></head>" + "\n" +
+          "  <body></body>" + "\n" +
           "</html>";
     assert.equal(main.toHtml(structureToConvert), expectedHtml);
   });
--- src/application/shared/main.js
+++ src/application/shared/main.js
@@ -1,6 +1,19 @@
 exports.toHtml = function toHtml(structureToConvert) {
   const htmlToReturn =
-        "<html>" + "\n" +
-        "</html>";
+    "<html>" + convertArrayToHtml(structureToConvert, 0) + "</html>";
   return htmlToReturn;
 };
+
+function convertArrayToHtml(htmlArray, indentLevel) {
+  const adjustedIndentLevel = indentLevel + 2;
+  return htmlArray.reduce(function(accumulatedString, elementArray) {
+    if (elementArray.length === 1) {
+      const elementTag = elementArray[0];
+      return (
+        accumulatedString +
+        " ".repeat(adjustedIndentLevel) +
+        `<${elementTag}></${elementTag}>\n`
+      );
+    }
+  }, "\n");
+}
