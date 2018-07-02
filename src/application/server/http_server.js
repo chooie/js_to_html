94676e47d25a4aad42bc6ca51da01732fce66bcc
chooie <charlie.hebert92@gmail.com>
2018-07-02 13:41:27 +0100

    Index page and 404 page are generated from js_to_html library

--- src/application/server/http_server.js
+++ src/application/server/http_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const util = require("util");
+
 const main = require("../shared/main.js");
+const myUtil = require("../shared/util.js");
 
 function makeTitle(title) {
   return ["title", title];
@@ -18,19 +20,42 @@ exports.make = function make(portNumber, contentDir, notFoundPageToServe) {
       "html",
       [
         "head",
-        "<!-- App home page -->",
+        "<!-- smoke test marker: App home page -->",
         makeTitle("Home - Automatopia NodeJS")
       ],
-      ["body", ["h1", { id: "header-text" }, "Hello, world!"]]
+      [
+        "body",
+        ["h1", { id: "header-text" }, "Hello, world!"],
+        ["script", { src: "bundle.js" }],
+        [
+          "script",
+          myUtil.stripMargin`
+          |const client = require("./main.js");
+          |console.log(client.isTrue());
+          |`
+        ]
+      ]
     ]);
-    res.status(400).send(contentHtml);
+    res.status(200).send(contentHtml);
   });
 
   httpServer.use(express.static(contentDir));
   httpServer.use(function(req, res, next) {
-    res
-      .status(404)
-      .render("404", { title: "Page Not Found - Automatopia NodeJS" });
+    res.set("Content-Type", "text/html");
+    const contentHtml = main.toHtml([
+      "html",
+      [
+        "head",
+        "<!-- smoke test marker: App 404 page -->",
+        makeTitle("Page Not Found - Automatopia NodeJS")
+      ],
+      [
+        "body",
+        ["h1", { id: "header-text" }, "404: Page Not Found :("],
+        ["a", { href: "/" }, "Go Home"]
+      ]
+    ]);
+    res.status(404).send(contentHtml);
   });
 
   return {
