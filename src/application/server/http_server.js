6c28785337a131a5548580c37bc33cf029b2510f
chooie <charlie.hebert92@gmail.com>
2018-07-02 14:26:41 +0100

    Separate pages to their own module

--- src/application/server/http_server.js
+++ src/application/server/http_server.js
@@ -1,60 +1,21 @@
 const express = require("express");
 const util = require("util");
 
-const main = require("../shared/main.js");
-const myUtil = require("../shared/util.js");
-
-function makeTitle(title) {
-  return ["title", title];
-}
+const pages = require("./pages.js");
 
 exports.make = function make(portNumber, contentDir, notFoundPageToServe) {
   const httpServer = express();
 
-  httpServer.set("views", "src/application/client/content/template_views");
-  httpServer.set("view engine", "pug");
-
   httpServer.get("/", function(req, res) {
     res.set("Content-Type", "text/html");
-    const contentHtml = main.toHtml([
-      "html",
-      [
-        "head",
-        "<!-- smoke test marker: App home page -->",
-        makeTitle("Home - Automatopia NodeJS")
-      ],
-      [
-        "body",
-        ["h1", { id: "header-text" }, "Hello, world!"],
-        ["script", { src: "bundle.js" }],
-        [
-          "script",
-          myUtil.stripMargin`
-          |const client = require("./main.js");
-          |console.log(client.isTrue());
-          |`
-        ]
-      ]
-    ]);
+    const contentHtml = pages.makeIndexPage();
     res.status(200).send(contentHtml);
   });
 
   httpServer.use(express.static(contentDir));
   httpServer.use(function(req, res, next) {
     res.set("Content-Type", "text/html");
-    const contentHtml = main.toHtml([
-      "html",
-      [
-        "head",
-        "<!-- smoke test marker: App 404 page -->",
-        makeTitle("Page Not Found - Automatopia NodeJS")
-      ],
-      [
-        "body",
-        ["h1", { id: "header-text" }, "404: Page Not Found :("],
-        ["a", { href: "/" }, "Go Home"]
-      ]
-    ]);
+    const contentHtml = pages.make404Page();
     res.status(404).send(contentHtml);
   });
 
